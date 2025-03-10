f5995263dc653f778b45a904660636cebffc09ee
Ostan Dsouza <oston.prithesh.dsouza-ext@continental.com>
2025-03-10 11:11:13 +0530

    beltforms class error fixed

--- src/test/java/com/web/steps/BeltFormsSteps.java
+++ src/test/java/com/web/steps/BeltFormsSteps.java
@@ -7,7 +7,7 @@ import com.web.pages.UsersPage;
 
 import static com.qmetry.qaf.automation.core.ConfigurationManager.getBundle;
 
-public class BeltFormsSteps {
+public class BeltformsSteps {
 
     LoginPage loginPage = new LoginPage();
     BeltFormsPge beltpage = new BeltFormsPge();
